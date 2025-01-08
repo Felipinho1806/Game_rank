51b981442c89accf5b815e69c5622fa3fd7fae01
OLi-byte <douglasso1206@gmail.com>
2025-01-08 10:37:19 -0300

    Integrate Spring Boot framework into the project

---
+++ src/main/java/com/olibyte_felipinho/game_ranking/GameRankingApplication.java
@@ -0,0 +1,13 @@
+package com.olibyte_felipinho.game_ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olibyte_felipinho/game_ranking/GameRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olibyte_felipinho.game_ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
